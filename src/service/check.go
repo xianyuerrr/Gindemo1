792eadcae7678bbc0567a32c96d83625286e2fa7
kguniverse <kkwang@outlook.com>
2021-11-04 19:48:25 +0800

    upd hit func skeleton

--- src/service/check.go
+++ src/service/check.go
@@ -4,9 +4,11 @@ import "demo1/src/model"
 
 func Hit(form model.Client) (string, string, string, string, string) {
 	//todo
+	rules := model.GetRules(form.DeviceId)
 
+	hitRule := rules[0]
 	//if hit,
 	//return downloadUrl, updateVersionCode, md5, title, updateTips
 
-	return "11", "22", "33", "44", "55"
+	return hitRule.DownloadUrl, hitRule.UpdateVersionCode, hitRule.Md5, hitRule.Title, hitRule.UpdateTips
 }
