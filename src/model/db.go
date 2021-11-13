8c1600a31a917064695e29f406224cfba57389ab
kguniverse <kkwang@outlook.com>
2021-11-13 14:28:09 +0800

    change dsn

--- src/model/db.go
+++ src/model/db.go
@@ -2,8 +2,6 @@ package model
 
 import (
 	"github.com/jinzhu/gorm"
-	"gopkg.in/yaml.v3"
-	"io/ioutil"
 )
 
 var Db *gorm.DB
@@ -19,17 +17,22 @@ type Config struct {
 }
 
 func Mysql() {
-	dbConfig := Config{}
-
-	data, err := ioutil.ReadFile("https://github.com/kguniverse/techtrainingcamp-AppUpgrade/blob/master/src/config.yml")
-	if err != nil {
-		panic(err)
-	}
-	err = yaml.Unmarshal([]byte(data), &dbConfig)
-	if err != nil {
-		panic(err)
-	}
-	db, err := gorm.Open("mysql", dbConfig.Mysql.Dsn)
+	//dbConfig := Config{}
+	//
+	//data, err := ioutil.ReadFile("https://github.com/kguniverse/techtrainingcamp-AppUpgrade/blob/master/src/config.yml")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//err = yaml.Unmarshal([]byte(data), &dbConfig)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//db, err := gorm.Open("mysql", dbConfig.Mysql.Dsn)
+	//if err != nil {
+	//	panic(err)
+	//}
+	Dsn := "kgkg:Wang0805@tcp(rm-bp17dut928o4el9fcvo.mysql.rds.aliyuncs.com:3306)/sys?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open("mysql", Dsn)
 	if err != nil {
 		panic(err)
 	}
